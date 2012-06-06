4d4a652273ac2bf3747538c0113cc1c979862e09
root <root@apothecary.(none)>
2012-06-06 02:53:16 -0400

    a checkin

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 echo "<p>Hello World</p>"; 
 $myFile = "php/payroll.txt";
 $fh = fopen($myFile, 'a') or die("can't open file");
-$stringData = "$_POST['width']\n";
+$stringData = "AWD\n";
 fwrite($fh, $stringData);
 fclose($fh);
 
