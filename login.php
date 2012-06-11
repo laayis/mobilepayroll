c8b81c89a56e8de9ec80ddd55a7f48fa4f99c80e
root <root@apothecary.(none)>
2012-06-10 23:12:09 -0400

    awdwa

--- login.php
+++ login.php
@@ -27,7 +27,7 @@ if($result){
 	setcookie('id', getEmployeeId($link), time()+3600);	
 	header('Location: pages/overview.php');
 } else{
-	header('Location: localhost');
+	header('http://helloworld123.elasticbeanstalk.com/');
 }
 
 ?>
