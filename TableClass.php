6ed192440741a2df946d600b29e6af341f18e77b
root <root@apothecary.(none)>
2012-06-22 02:14:17 -0400

    afirst launch 1

--- TableClass.php
+++ TableClass.php
@@ -367,7 +367,7 @@ if(isset($nextday['look']) == 1){
 		//add last punchout
 		array_shift($punches);
 	} else{
-		array_pop($punches);
+		$punches = array_pop($punches);
 	}
 	//if even do nothing, if odd then pop to make it even
 	//*
