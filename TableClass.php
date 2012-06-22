8ac358cea0a9837bea917eeb1884f0380aad128d
root <root@apothecary.(none)>
2012-06-22 02:20:14 -0400

    afirst launch 1

--- TableClass.php
+++ TableClass.php
@@ -370,6 +370,9 @@ if(isset($nextday['look']) == 1){
 		array_pop($punches);
 		print_r($punches);
 	}
+	if(count($punches) %2 == 1){
+		array_pop($punches)
+	}
 	//if even do nothing, if odd then pop to make it even
 	//*
 	
