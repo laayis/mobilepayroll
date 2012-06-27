6f6fae6178c5876efb5d4cfcb41b660b177cc687
root <root@apothecary.(none)>
2012-06-26 22:22:41 -0400

    afirst launch 1

---
+++ app/testclock.php
@@ -0,0 +1,16 @@
+<html>
+<head><title>Test Form</title></head>
+<body>
+<?php
+include('AppClass.php');
+
+?>
+
+<form method='POST' action='clockuser.php'>
+<input name='totable_license' type='text' value='900e-d807-409a-4d9d'/>
+<input name='totable_date' type='text' value='2012-12-06 06:56:00'/>
+<input name='totable_id' type='text' value='7'/>
+<input type='submit' value='submit' />
+</form>
+</body>
+</html>
