d2bcc8f21d9a761d684a9738aca4244a9f0fcc18
root <root@apothecary.(none)>
2012-06-26 23:28:48 -0400

    afirst launch 1

--- admin/pages/overview_c.php
+++ admin/pages/overview_c.php
@@ -4,10 +4,10 @@ Select a function at the left by clicking on an activity you'd like to complete.
 
 <div style="width: 100px; padding: 15px; margin-top: 25px;  background-color: #CCCCCC; border-radius: 5px; -moz-border-radius: 5px; text-align: center;">
 <div class="app_image">
-	<a href="/TimeSheet.apk"><img src="../../images/android.png" width="100"></a>
+	<a href="/Park.apk"><img src="../../images/android.png" width="100"></a>
 </div>
 <div style="margin-top: 5px; font-style: bold;" class="app_text">
-	<a href="/TimeSheet.apk">Download TimeSheet APK.</a>
+	<a href="/Park.apk">Download Park APK.</a>
 </div>
 </div>
 
