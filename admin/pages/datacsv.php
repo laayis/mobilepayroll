0f3a27ed05e841c8a5c2c5e5ad3e9763e9e3c419
mystix24 <winicius@gmail.com>
2012-07-07 02:24:44 -0400

    Add grapher to plot employee count, employee checks, etc.

--- admin/pages/datacsv.php
+++ admin/pages/datacsv.php
@@ -22,24 +22,6 @@ if(isset($_POST['request'])){
 		echo $date . '
 ';
 		echo $count;
-	} else if($_POST['request']=='pay'){
-		$link = initDb();
-		selectDb($link);
-
-		$query = "SELECT date, count FROM history_pay WHERE company_id='{$id}' ORDER BY date ASC";
-		$result = queryDbAll($link, $query);
-		
-		$date = 'Categories,';
-		$count = showCompanyName($link, $id).',';
-		foreach($result as $value){
-			$date .= $value[0] . ',';
-			$count .= $value[1] . ',';
-		}
-		$date = rtrim($date, ',');
-		$count = rtrim($count, ',');
-		echo $date . '
-';
-		echo $count;
 	}
 }
 
